98565f4012bf6a6ff9c14a26dcd829259e3285b4
Mark Baggett <mbagget1@utk.edu>
2021-12-14 14:06:18 -0500

    Add location information to MODS.

--- generate.py
+++ generate.py
@@ -20,12 +20,15 @@ class OAIGenerator:
 class MODS:
     """Generates MODS from a UTK IIIF Manifest."""
     def __init__(self, manifest):
-        self.manifest = manifest
+        self.manifest_id = manifest
+        self.manifest = requests.get(self.manifest_id).json()
         self.slug = self.__choose_slug()
+        self.pid = self.__get_pid()
+        self.mods_path = f"https://digital.lib.utk.edu/collections/islandora/object/{self.pid}/datastream/MODS"
+        self.__get_islandora_mods()
 
     def __choose_slug(self):
-        r = requests.get(self.manifest).json()
-        label = r['label']['en'][0]
+        label = self.manifest['label']['en'][0]
         slugified_label = label.lower().replace('interview with ', '').replace(',','').replace(' ','-')
         return f"https://rfta.lib.utk.edu/interviews/object/{slugified_label}/"
 
@@ -35,7 +38,23 @@ class MODS:
             print(self.slug)
         return
 
+    def __get_pid(self):
+        split_value = self.manifest_id.split('/')
+        return(f"{split_value[5]}:{split_value[6]}")
+
+    def __get_islandora_mods(self):
+        r = requests.get(self.mods_path)
+        original_mods = r.content.decode('utf-8')
+        mods_minus_close = original_mods.replace('</mods>\n', '').replace('</mods>', '')
+        final_mods = f"{mods_minus_close}\n{self.__add_location_node()}\n</mods>\n"
+        print(final_mods)
+
+    def __add_location_node(self):
+        node = f"""   <location>\n      <physicalLocation>University of Tennessee, Knoxville. Special Collections</physicalLocation>\n      <url access="object in context" usage="primary display">{self.slug}</url>\n      <url access="preview">https://digital.lib.utk.edu/collections/islandora/object/{self.pid}/datastream/TN/view</url>\n   </location>"""
+        return node
 
 if __name__ == "__main__":
-    collection = "https://digital.lib.utk.edu/static/iiif/collections/rfta_completed.json"
-    x = OAIGenerator(collection)
+    # collection = "https://digital.lib.utk.edu/static/iiif/collections/rfta_completed.json"
+    # x = OAIGenerator(collection)
+    mods = MODS('https://digital.lib.utk.edu/assemble/manifest/rfta/6')
+    print(mods.pid)
